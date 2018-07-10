e5f7fbe44d9f7fbc37d22834018213c0e851c8ef
heinz <james_nirvana@gmx.com>
2018-07-10 15:07:06 +0300

    CHAINPARAMSSEEDS_H

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -7,7 +7,10 @@
  * Each line contains a 16-byte IPv6 address and a port.
  * IPv4 as well as onion addresses are wrapped inside a IPv6 address accordingly.
  */
-static SeedSpec6 pnSeed6_main[] = {};
+static SeedSpec6 pnSeed6_main[] = {
+	{{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xff,0xff,0x68,0xee,0xbe,0x33}, 9887}
+	
+};
 
 static SeedSpec6 pnSeed6_test[] = {};
 #endif // BITCOIN_CHAINPARAMSSEEDS_H
