5bed215ec8395eae80bb0b21187ccb7b66a61c97
TFinch <travisfinch01@gmail.com>
2018-07-16 19:12:45 -0400

    Test

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -183,7 +183,7 @@ public:
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
         fRequireRPCPassword = true;
-        fMiningRequiresPeers = true;
+        fMiningRequiresPeers = false;  //Was true
         fAllowMinDifficultyBlocks = false;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
