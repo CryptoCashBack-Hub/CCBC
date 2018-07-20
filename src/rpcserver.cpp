562c29e881ee45d4d7b0c5bdfc41971d0a97ebb7
TFinch <travisfinch01@gmail.com>
2018-07-20 15:05:34 -0400

    Port Fix from 6970 to 19112 (#12)

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -1093,7 +1093,7 @@ std::string HelpExampleRpc(string methodname, string args)
 {
     return "> curl --user myusername --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", "
            "\"method\": \"" +
-           methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:6970/\n";
+           methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:19112/\n";
 }
 
 const CRPCTable tableRPC;
