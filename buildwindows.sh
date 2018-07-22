bc054b66ac7579da56f7683706121662af496efd
MotoAcidic <travisfinch01@gmail.com>
2018-07-22 13:52:53 -0400

    Installs

--- buildwindows.sh
+++ buildwindows.sh
@@ -1,12 +1,23 @@
-#!/bin/bash
 sudo apt update
 sudo apt upgrade
 sudo apt install build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git
-sudo update-alternatives --config x86_64-w64-mingw32-g++ # Set the default mingw32 g++ compiler option to posix.
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev
+sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+sudo apt-get install libzmq3-dev -y
+sudo apt-get install libminiupnpc-dev -y
+sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev openssl -y
+
+sudo apt install g++-mingw-w64-x86-64
+sudo update-alternatives --config x86_64-w64-mingw32-g++ # Set the default mingw32 g++ compiler option to posix. or choose 0 for deb8
 PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g') # strip out problematic Windows %PATH% imported var
+git clone https://github.com/SMRT-Cloud/smrtc.git
 cd depends
 make HOST=x86_64-w64-mingw32
 cd ..
-./autogen.sh # not required when building from tarball
-CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/
-make
+
+bash build-win64.sh
