2efc7502d9c5b7f71ecdd22305527ed2074cb7c6
TFinch <travisfinch01@gmail.com>
2018-07-23 14:12:00 -0400

    Update buildwindows.sh

--- buildwindows.sh
+++ buildwindows.sh
@@ -15,7 +15,6 @@ sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev open
 sudo apt install g++-mingw-w64-x86-64
 sudo update-alternatives --config x86_64-w64-mingw32-g++ # Set the default mingw32 g++ compiler option to posix. or choose 0 for deb8
 PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g') # strip out problematic Windows %PATH% imported var
-git clone https://github.com/SMRT-Cloud/smrtc.git
 cd depends
 make HOST=x86_64-w64-mingw32
 cd ..
