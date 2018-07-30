3a61a4f7ad54cb46c00140234388bdc6da26cf6e
TFinch <travisfinch01@gmail.com>
2018-07-30 14:16:23 -0400

    New Genesis

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -56,10 +56,10 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 // + Contains no strange transactions
 static Checkpoints::MapCheckpoints mapCheckpoints =
     boost::assign::map_list_of
-    (0, uint256("0x6e8046efd226dcad93ad7afe45540539b73df48872a9e9ac69ca866e114c6305"));
+    (0, uint256("0x00000dca344702a588a4c4aa2ca20fc2239c854ae0847f9bb90c3d049232f059"));
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1532961278, // * UNIX timestamp of last checkpoint block
+    1532974494, // * UNIX timestamp of last checkpoint block
     0,    // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     2000        // * estimated number of transactions per day after checkpoint, was 2000
@@ -130,7 +130,7 @@ public:
         nModifierUpdateBlock = 1; //Was 999999999
         nZerocoinStartHeight = 201;
         nAccumulatorStartHeight = 1;
-        nZerocoinStartTime = 1532961278; // 07/25/2018 @ 5:45pm (UTC)
+        nZerocoinStartTime = 1532974494; // 07/25/2018 @ 5:45pm (UTC)
         nBlockEnforceSerialRange = 1; //Enforce serial range starting this block
         nBlockRecalculateAccumulators = ~1; //Trigger a recalculation of accumulators
         nBlockFirstFraudulent = ~1; //First block that bad serials emerged
@@ -157,17 +157,17 @@ public:
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         genesis.nVersion = 1;
-        genesis.nTime = 1532961278;
+        genesis.nTime = 1532974494;
         genesis.nBits = 504365040;
-        genesis.nNonce = 443130;
+        genesis.nNonce = 998842;
 
 				
         hashGenesisBlock = genesis.GetHash();
 		
 		printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
         printf("genesis.hashMerkleRoot = %s\n", genesis.hashMerkleRoot.ToString().c_str());
-        assert(hashGenesisBlock == uint256("0x6e8046efd226dcad93ad7afe45540539b73df48872a9e9ac69ca866e114c6305"));
-        assert(genesis.hashMerkleRoot == uint256("0x210d76e67d9e83752ca508ac414122aa320837a9aacb4925d70e65a066cb9abc"));
+        assert(hashGenesisBlock == uint256("0x00000dca344702a588a4c4aa2ca20fc2239c854ae0847f9bb90c3d049232f059"));
+        assert(genesis.hashMerkleRoot == uint256("0x4210d3070e7d3f90e54948df319f1bbbf5b1d212015f160e2bdc2729a8dbfe25"));
 
 		
 		
