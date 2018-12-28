ebadef1cc34e0196a046e476ee8eba54114fddfb
TFinch <travisfinch01@gmail.com>
2018-12-27 19:21:19 -0500

    Final install scripts

--- scripts/build-win32.sh
+++ scripts/build-win32.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-cd ..
-sudo apt install g++-mingw-w64-i686 mingw-w64-i686-dev
-PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g') # strip out problematic Windows %PATH% imported var
-cd depends
-make HOST=i686-w64-mingw32
 cd ..
 ./autogen.sh
 ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/i686-w64-mingw32
