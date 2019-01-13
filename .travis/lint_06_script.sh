ddcaadeead72828fcd9980a7d368467cd845a6d1
Unknown <info@aviator-coding.de>
2019-01-13 13:19:17 -0500

    Travis Permission

--- .travis/lint_06_script.sh
+++ .travis/lint_06_script.sh
@@ -6,7 +6,7 @@
 
 export LC_ALL=C
 
-
+chmod +x contrib/devtools/git-subtree-check.sh
 contrib/devtools/git-subtree-check.sh src/secp256k1
 contrib/devtools/git-subtree-check.sh src/univalue
 contrib/devtools/git-subtree-check.sh src/leveldb
