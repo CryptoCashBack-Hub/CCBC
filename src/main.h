015db48fbea1fe1c787bf2e114504b0d54785044
Travis <travisfinch01@gmail.com>
2019-06-15 11:50:03 -0400

    Missed IsRevivedBlock

--- src/main.h
+++ src/main.h
@@ -243,8 +243,12 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
 
 bool ActivateBestChain(CValidationState& state, CBlock* pblock = NULL, bool fAlreadyChecked = false);
 CAmount GetBlockValue(int nHeight);
+
+//Devfee Related
 bool IsTreasuryBlock(int nHeight);
 int64_t GetTreasuryAward(int nHeight);
+bool IsReviveBlock(int nHeight);
+int64_t GetReviveAward(int nHeight);
 
 /** Create a new block index entry for a given block hash */
 CBlockIndex* InsertBlockIndex(uint256 hash);
